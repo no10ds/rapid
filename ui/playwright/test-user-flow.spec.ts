c1fcdbcf8fe4086eacff8954e2c6c58f2fdc9aac
Toby Drane <tdrane@no10.gov.uk>
2023-09-01 16:53:34 +0100

    remove hardcoded subject id

--- ui/playwright/test-user-flow.spec.ts
+++ ui/playwright/test-user-flow.spec.ts
@@ -1,9 +1,7 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { test, expect } from '@playwright/test'
 
-import { makeAPIRequest, generateRapidAuthToken } from './utils'
-
-const domain = 'http://localhost:3000'
+import { makeAPIRequest, generateRapidAuthToken, domain } from './utils'
 
 const user = `${process.env.RESOURCE_PREFIX}_ui_test_user`
 
@@ -11,7 +9,7 @@ test('test', async ({ page }) => {
   await page.goto(domain)
 
   // Modify user to have data admin permissions
-  await page.locator('div[role="button"]:has-text("Modify User")').click()
+  https: await page.locator('div[role="button"]:has-text("Modify User")').click()
   await expect(page).toHaveURL(`${domain}/subject/modify`)
   await page.locator('[data-testid="field-user"]').selectOption({ label: user })
   await page.locator('[data-testid="submit-button"]').click()
@@ -28,20 +26,22 @@ test('test', async ({ page }) => {
   // Test unique condition where we correctly display permissions when modifying a user
   // even though they might have conflicting permissions within the filtering logic
   const { access_token } = await generateRapidAuthToken()
+  const url = page.url()
+  const subjectId = url.split('/').pop()
   await makeAPIRequest(
     'subjects/permissions',
     'PUT',
     {
-      subject_id: 'b10ded88-4e10-46d3-b9c7-ff6cf0526c09',
+      subject_id: subjectId,
       permissions: [
         'DATA_ADMIN',
         'READ_ALL',
         'USER_ADMIN',
         'WRITE_ALL',
-        'READ_DEFAULT_PROTECTED_TEST_E2E_PROTECTED',
-      ],
+        'READ_DEFAULT_PROTECTED_TEST_E2E_PROTECTED'
+      ]
     },
-    `Bearer ${access_token}`,
+    `Bearer ${access_token}`
   )
   await page.locator('div[role="button"]:has-text("Modify User")').click()
   await expect(page).toHaveURL(`${domain}/subject/modify`)
