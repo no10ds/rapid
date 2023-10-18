54bd62eb8efadb212e1651fdb672cc45ad487a16
Toby Drane <tdrane@no10.gov.uk>
2023-10-18 22:47:11 +0100

    bump rAPId wide versions

--- infrastructure/modules/rapid/variables.tf
+++ infrastructure/modules/rapid/variables.tf
@@ -13,13 +13,13 @@ variable "app-replica-count-max" {
 variable "application_version" {
   type        = string
   description = "The version number for the application image (e.g.: v1.0.4, v1.0.x-latest, etc.)"
-  default     = "v7.0.5"
+  default     = "v7.0.6"
 }
 
 variable "ui_version" {
   type        = string
   description = "The version number for the static ui (e.g.: v1.0.0, etc.)"
-  default     = "v7.0.5"
+  default     = "v7.0.6"
 }
 
 variable "catalog_disabled" {
