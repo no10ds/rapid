f109fbcb8225a045a1f26866314610fa11b0957f
mohit <MMotwani@no10.gov.uk>
2024-01-25 12:52:35 +0000

    see if sdk_test can be brought back

--- sdk/setup.py
+++ sdk/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="rapid-sdk",
-    version="0.1.6",
+    version=os.getenv("TEST_SDK_VERSION", "0.1.6"),
     description="A python sdk for the rAPId API",
-    url="https://github.com/no10ds/rapid-sdk",
+    url="https://github.com/no10ds/rapid/tree/main/sdk",  # Originally pointed to a deprecated repo, have updated
     author="Lewis Card",
     author_email="lcard@no10.gov.uk",
     license="MIT",
