0e4a1dd30df837adb6ecdd025f1b512666c42aa4
mohit <MMotwani@no10.gov.uk>
2024-02-01 10:59:57 +0000

    adding quick fix to setup.py

--- sdk/setup.py
+++ sdk/setup.py
@@ -3,7 +3,10 @@ import os
 
 setup(
     name="rapid-sdk",
-    version=os.getenv("TEST_SDK_VERSION", "0.1.6"),
+    # version=os.getenv("TEST_SDK_VERSION", "0.1.6"),
+    version="0.1.6"
+    if os.getenv("TEST_SDK_VERSION") is None
+    else ("0.1.6" + os.getenv("TEST_SDK_VERSION")),
     description="A python sdk for the rAPId API",
     url="https://github.com/no10ds/rapid/tree/main/sdk",  # Originally pointed to a deprecated repo, have updated
     author="Lewis Card",
