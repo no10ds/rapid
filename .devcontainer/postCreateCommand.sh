7694ffae2f4bd25300303148a39f7a052b347c0e
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2024-11-21 14:09:25 +0000

    update container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,24 @@
 pip install -r ./.devcontainer/requirements.txt
+pip install -r ./requirements.txt
+
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+apt-get update
+apt-get -y install gh
+apt install -y jq
+
+gh auth login
 
 make python-setup
 make api/setup
+
+pip install -r ./sdk/requirements.txt
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 make node-setup
 make ui/setup
