c7af1c2cbfc20cd404ca146ce7aece150d252381
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2024-11-25 11:32:43 +0000

    remove devcontainer folder

--- .devcontainer/postStartCommand.sh
+++
@@ -1,3 +0,0 @@
-git config --global --unset http.sslcainfo | exit 0
-git update-index --assume-unchanged .devcontainer/devcontainer.json   
-# git update-index --no-assume-unchanged .devcontainer/devcontainer.json
