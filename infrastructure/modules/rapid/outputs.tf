20d604258297a11367e43f77c6c887f7d5c9065c
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2025-02-18 09:41:14 +0000

    add hosted zone id to outputs

--- infrastructure/modules/rapid/outputs.tf
+++ infrastructure/modules/rapid/outputs.tf
@@ -7,3 +7,8 @@ output "catalogue_db_name" {
   value       = module.data_workflow.catalogue_db_name
   description = "The name of the Glue Catalogue database"
 }
+
+output "hosted_zone_id" {
+  value       = var.hosted_zone_id != "" ? var.hosted_zone_id : module.app_cluster.hosted_zone_id
+  description = "The hosted zone id"
+}
