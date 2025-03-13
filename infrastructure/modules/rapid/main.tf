0c7fc316a930fdd459c00e39d7b280ef7538b401
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2025-03-13 11:24:51 +0000

    remove acl for buckets having another control reso

--- infrastructure/modules/rapid/main.tf
+++ infrastructure/modules/rapid/main.tf
@@ -89,11 +89,6 @@ resource "aws_s3_bucket" "this" {
 
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
