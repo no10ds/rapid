481818b60fe39c28a468326479c5ac63790876ad
mmotwani@no10.gov.uk <mmotwani@no10.gov.uk>
2025-08-26 16:43:25 +0100

    Allow SES injection into auth module

--- infrastructure/blocks/auth/main.tf
+++ infrastructure/blocks/auth/main.tf
@@ -6,15 +6,7 @@ terraform {
 
 module "auth" {
   source = "../../modules/auth"
-
   tags                       = var.tags
   domain_name                = var.domain_name
   resource-name-prefix       = var.resource-name-prefix
-  aws_region                 = var.aws_region
-  allowed_email_domains      = var.allowed_email_domains
-  aws_account                = var.aws_account
-  cognito_ses_authentication = var.cognito_ses_authentication
-  ses_allowed_from_emails    = var.ses_allowed_from_emails
-  hosted_zone_id             = var.hosted_zone_id
-  ses_email_notifications    = var.ses_email_notifications
 }
