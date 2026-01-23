7bc49d846d0c21d31fee86770e216e83edf60702
Abigail Muller <amuller@no10.gov.uk>
2026-01-23 16:12:08 +0000

    Remove unsafe-inline from csp

--- backend/api/entry.py
+++ backend/api/entry.py
@@ -242,7 +242,7 @@ def _set_security_headers(response) -> None:
     response.headers["Content-Security-Policy"] = (
         "default-src 'self' "
         f"{IDENTITY_PROVIDER_BASE_URL}; "
-        "script-src 'self' 'unsafe-inline'; "
+        "script-src 'self'; "
         "style-src 'self'; "
         "img-src 'self' data: "
         "fastapi.tiangolo.com/img/favicon.png;"
