b69768edffb4b5b77f37cefbefa48cf61bf581ec
Abigail Muller <amuller@no10.gov.uk>
2026-02-05 14:10:33 +0000

    rate limiting waf rule

--- infrastructure/modules/ui/waf.tf
+++ infrastructure/modules/ui/waf.tf
@@ -207,6 +207,27 @@ resource "aws_wafv2_web_acl" "rapid_acl" {
     }
   }
 
+  rule {
+    name     = "RateLimitRule"
+    priority = 4
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 5000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitRule"
+      sampled_requests_enabled   = true
+    }
+  }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
