ab5943e2d279de2c12d8255abeea0852a563728e
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2026-06-16 13:15:13 +0100

    add kms perm to delete key

--- infrastructure/modules/app-cluster/cloudtrail.tf
+++ infrastructure/modules/app-cluster/cloudtrail.tf
@@ -19,6 +19,7 @@ data "aws_iam_policy_document" "access_logs_key_policy" {
       "kms:Encrypt",
       "kms:Decrypt",
       "kms:ReEncrypt*",
+      "kms:ScheduleKeyDeletion"
     ]
 
     resources = [
